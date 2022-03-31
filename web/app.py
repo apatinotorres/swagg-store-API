4c1f8c24df7bb17483ed63c04a113cf8638f59e4
EdwinPuertas <eapuerta@gmail.com>
2022-03-31 16:31:31 -0500

    Update pet operations

--- web/app.py
+++ web/app.py
@@ -18,28 +18,8 @@ def person():
     return render_template('new_pet.html')
 
 
-@app.route('/pet', methods=['GET'])
-def pets(status: str = "available"):
-    url2 = "https://petstore.swagger.io/v2/pet/findByStatus?status={0}".format(status)
-    response = requests.get(url2)
-    print(response.json())
-    data_pets = [(i['id'], i['name'], i['status'], i['photoUrls']) for i in response.json()]
-    print(data_pets)
-    return render_template('pets.html', value=data_pets)
-
-
-@app.route('/pet_update/<id_pet>', methods=['GET'])
-def pet_update(id_pet):
-    return render_template('pet_update.html', id_pet=id_pet)
-
-
-@app.route('/pet_delete/<id_pet>', methods=['GET'])
-def pet_delete(id_pet):
-    return render_template('pet_detail.html', id_pet=id_pet)
-
-
 @app.route('/pet_detail', methods=['POST'])
-def person_detail():
+def pet_detail():
     api_url = "https://petstore.swagger.io/v2/pet"
     id = request.form['id']
     name = request.form['name']
@@ -62,8 +42,57 @@ def person_detail():
                     "status": "available"
                 }
     response = requests.post(api_url, json=new_data)
-    print(response.json())
     return render_template('pet_detail.html', value=(id, name))
+
+
+@app.route('/pet', methods=['GET'])
+def pets(status: str = "available"):
+    url2 = "https://petstore.swagger.io/v2/pet/findByStatus?status={0}".format(status)
+    response = requests.get(url2)
+    data_pets = [(i['id'], i['name'], i['status']) for i in response.json()]
+    return render_template('pets.html', value=data_pets)
+
+
+@app.route('/pet_update/<id_pet>', methods=['GET'])
+def pet_update(id_pet):
+    return render_template('pet_update.html', value=id_pet)
+
+
+@app.route('/pet_update_detail', methods=['POST'])
+def pet_update_detail():
+    api_url_update = "https://petstore.swagger.io/v2/pet"
+    id = request.form['id']
+    name = request.form['name']
+    status = request.form['status']
+    update_data = {
+                        "id": id,
+                        "category": {
+                            "id": 0,
+                            "name": "string"
+                        },
+                        "name": name,
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 0,
+                                "name": "string"
+                            }
+                        ],
+                        "status": status
+                    }
+    response = requests.post(api_url_update, json=update_data)
+    return render_template('pet_detail.html', value=(id, name, status))
+
+
+@app.route('/pet_delete/<id_pet>', methods=['GET'])
+def pet_delete(id_pet):
+    api_url = "https://petstore.swagger.io/v2/pet/{0}".format(id_pet)
+    response = requests.delete(api_url)
+    return render_template('pet_detail.html', value="Delete successfully")
+
+
 
 '''
 @app.errorhandler(404)
