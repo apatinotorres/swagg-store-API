4ed98cb8f2e2a517a268240eabcfa4fc75f6479b
apatinotorres <antoniojosepatino@gmail.com>
2022-04-18 08:17:55 -0500

    agregado users

--- web/app.py
+++ web/app.py
@@ -92,6 +92,9 @@ def pet_delete(id_pet):
     response = requests.delete(api_url)
     return render_template('pet_detail.html', value="Delete successfully")
 
+### Store
+
+
 @app.route('/store', methods=['GET'])
 def show_store():
     return render_template("store.html")
@@ -104,10 +107,83 @@ def show_inventory():
     # print(data)
     return render_template('inventory.html', value = data)
 
+@app.route('/place_order/<id_pet>', methods=['GET'])
+def place_order(id_pet):
+    api_url_order = "https://petstore.swagger.io/v2/store/order"
+    randomnumber = random.randint(1, 10)
+    orders = {
+        "id": randomnumber,
+        "petId": id_pet,
+        "quantity": 1,
+        "shipDate": "2023-04-02T15:40:56.635Z",
+        "status": "placed",
+        "complete": "true"
+    }
+    response = requests.post(api_url_order, json=orders)
+    return render_template('place_order.html', value=orders)
+
+@app.route('/store/find_order')
+def find_order():
+    order_id = request.form['id']
+    api_url_find_order = "https://petstore.swagger.io/v2/store/order/{0}".format(order_id)
+    response = requests.get(api_url_find_order)
+    order_found = response.json()
+    return render_template('find_order.html', value=order_found)
+
+@app.route('/order_delete', methods=["POST"])
+def order_delete():
+    order_id = request.form['id']
+    delete_url = "https://petstore.swagger.io/v2/store/order/{0}".format(order_id)
+    response = requests.delete(delete_url)
+    return render_template('order_delete.html')
 
 
 
 
+
+### User
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    url = 'https://petstore.swagger.io/v2/user'
+    email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+    id = random.randint(1, 1000)
+    model_user = {
+        "id": id,
+        "username": username,
+        "firstName": "mori",
+        "lastName": "mori",
+        "email": email,
+        "password": password,
+        "phone": "3243",
+        "userStatus": 0
+    }
+    response = requests.post(url, json=model_user)
+    return render_template('create_user.html', value=model_user)
+
+
+@app.route('/user', methods=['GET'])
+def user():
+    return render_template('user.html')
+
+
+@app.route('/user_log', methods=['GET', 'POST'])
+def user_log():
+    username = request.form['username']
+    password = request.form['password']
+    url = 'https://petstore.swagger.io/v2/user/login?username={0}&password={1}'.format(username, password)
+    response = requests.get(url)
+    user_info = response.json()
+    return render_template('user_log.html', value = user_info)
+
+
+@app.route('/log_out', methods=['GET'])
+def log_out():
+    url = 'https://petstore.swagger.io/v2/user/logout'
+    response = requests.get(url)
+    return render_template('log_out.html', value='Salida exitosa')
 
 '''
 @app.errorhandler(404)
